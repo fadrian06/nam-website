4056fc74b95fc23ebeaa16a6a432e424d0b150d6
fadrian06 <franyeradriansanchez@gmail.com>
2024-12-21 22:46:54 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Network Addon Mod - Documentación Oficial</title>
+    <base href="<?= str_replace('index.php', '', $_SERVER['SCRIPT_NAME']) ?>" />
+    <link rel="icon" href="./assets/images/favicon.png" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/customizations.css" />
+  </head>
+  <body>
+    <header class="sticky-top"></header>
+    <main class="container text-center col-md-8">
+      <section>
+        <h1 class="fw-bolder">Network Addon Mod</h1>
+        <h2 class="lead">SimCity 4 Network Addon Mod (NAM) por el Equipo NAM</h2>
+        <a class="btn btn-primary">Empezar - Primera lectura</a>
+        <img src="./assets/images/20yearsSC4.jpg" class="img-fluid my-2" />
+        <p>
+          Gracias por descargar el <strong>Network Addon Mod (NAM)</strong>
+          para SimCity 4 Deluxe (o SimCity 4 con la Expansión Hora Punta).
+        </p>
+        <p>
+          El NAM es una modificación de transporte completo que agrega el
+          equivalente a varios paquetes de expansión de nuevo contenido de
+          transporte al juego. Además de las correcciones a la simulación de
+          tráfico del juego y las características a la red base, agrega
+          innumerables características nuevas a las herramientas de red
+          existentes, como nuevos pasos elevados, rampas de acceso a las
+          autopistas, intersecciones, carreteras más anchas, vías peatonales,
+          tren ligero terrestre arrastrable, rotondas y carriles de giro,
+          entre muchas otras características.
+        </p>
+        <p>
+          Tenga en cuenta que esta documentación aún se encuentra en una etapa
+          de transición y algunos enlaces pueden hacer referencia a piezas de
+          documentación más antiguas, pero aún en gran medida relevantes.
+        </p>
+        <p>
+          La versión más reciente de esta documentación siempre se puede
+          encontrar en
+          <a href="https://www.sc4nam.com" target="_blank">
+            https://www.sc4nam.com
+          </a>.
+        </p>
+        <p class="fw-bold">
+          ¡Celebrando 20 años de SimCity 4 (14 de Enero del 2003 - 14 de Enero
+          del 2023)!
+        </p>
+      </section>
+      <hr />
+      <section>
+        <h3 class="fw-bold">Empezar</h3>
+        <p>
+          ¿No estás seguro por dónde empezar? Aprende más sobre cómo el Network
+          Addon Mod puede ser instalado, preguntas frecuentes, su historia y
+          las personas que hicieron que pasara.
+        </p>
+        <div class="btn-group">
+          <a class="btn btn-outline-primary">Empieza Aquí</a>
+          <a class="btn btn-outline-primary">Instalación</a>
+          <a class="btn btn-outline-primary">Lo Básico</a>
+          <a class="btn btn-outline-primary">Vista General</a>
+          <a class="btn btn-outline-primary">Preguntas Frecuentes</a>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <h3 class="fw-bold">Referencia</h3>
+        <div>
+          <a>Glosario</a>
+          <a>Historial de Versiones</a>
+          <a>Información Técnica</a>
+          <a>Créditos</a>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <h3 class="fw-bold">Guías de Características</h3>
+        <p>
+          ¿No estás seguro de como funciona una característica en particular?
+          Aprende más sobre cómo los componentes individuales del NAM y cómo
+          tomar ful ventaja de ellos en SimCity 4
+        </p>
+        <div>
+          <a>Complemento de Vehículos</a>
+          <a>Añadidos a la Redes Base</a>
+          <a>Puentes</a>
+          <a>Canales</a>
+          <a>Añadidos Cosméticos</a>
+          <a>Complementos de Vista de Datos</a>
+          <a>Pasos Subterráneos Flexibles</a>
+          <a>Tren de Alta Velocidad</a>
+          <a>Ferrocarril Híbrido</a>
+          <a>Tren ligero / Tranvía</a>
+          <a>Reemplazo de la Autopista Maxis</a>
+          <a>Utilidades</a>
+          <a>Network Widening Mod</a>
+          <a>Tren Antíguo</a>
+          <a>Modificación de Revolución Peatonal</a>
+          <a>Real Railway (RRW)</a>
+          <a>Real Expressway Mod</a>
+          <a>Real Highway Mod</a>
+          <a>Viaductos de Carretera</a>
+          <a>Rotondas</a>
+          <a>Carreteras Rurales</a>
+          <a>Estaciones</a>
+          <a>Modificación de Añadidos a Calles</a>
+          <a>El Simulador de Tráfico</a>
+          <a>Carriles de Giro</a>
+          <a>Ferrocarril Subterráneo</a>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <h3 class="fw-bold">Datos Técnicos</h3>
+        <p>
+          ¿Tienes curiosidad en como funcionan las partes internas del NAM?
+          Toma un vistazo.
+        </p>
+        <div>
+          <a>Ferrocarril Híbrido</a>
+          <a>Network Widening Mod</a>
+          <a>Real Highway Mod</a>
+          <a>Viaductos de Carretera</a>
+          <a>Modificación de Añadidos a Calles</a>
+          <a>Underground RUL Flags</a>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <h3 class="fw-bold">Advertencia</h3>
+        <p>
+          Users download, install, and run this mod completely and solely at
+          their own risk. Electronic Arts (EA), the creators, and its
+          individual contributors, are not responsible for any errors, crashes,
+          problems, or any other issue that you may have if you have downloaded
+          and applied this mod file to your game. Players should also expect
+          that any future patches and/or expansion packs and SimCityscape may
+          not function properly with the game if you have downloaded this mod
+          and applied it to your game. The use of this document, the
+          information within, and the Network Addon Mod is conditional upon the
+          acceptance of this disclaimer and all that is within this document.
+        </p>
+        <h3 class="fw-bold">A Big Thank You</h3>
+        <p>
+          Thanks goes first and foremost to the SC4 fansite admins, for
+          creating and laboring over their sites in order to make them a
+          vibrant SimCity 4 community. Without them, none of this would be
+          possible. A very special thank you goes to the entire SC4 community
+          and all the members who have contributed to this Network Addon Mod in
+          its current form.
+        </p>
+      </section>
+    </main>
+    <footer>
+      Copyright © 2008-<?= date('Y') ?> NAM Team
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
